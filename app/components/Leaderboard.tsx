7cec19edee386694add723bb513eeeaa6fb1b393
Dziudita <Stakesheep.team@gmail.com>
2025-04-03 19:10:46 +0300

    Update Leaderboard.tsx

--- app/components/Leaderboard.tsx
+++ app/components/Leaderboard.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 
-const REFRESH_INTERVAL = 30 * 60 * 1000; // 30 minutes
+const REFRESH_INTERVAL = 30 * 60 * 1000; // 30 minutes in milliseconds
 
 type User = {
   username?: string;
