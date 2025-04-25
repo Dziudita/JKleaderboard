73b4bdf43c312c0aa5589b28c176cd366a92f48b
Dziudita <Stakesheep.team@gmail.com>
2025-04-25 21:21:15 +0300

    Update Leaderboard.tsx

--- app/components/Leaderboard.tsx
+++ app/components/Leaderboard.tsx
@@ -91,17 +91,18 @@ export default function Leaderboard() {
     name.length < 5 ? name.slice(0, 2) + '***' : name.slice(0, 3) + '***' + name.slice(-1);
 
   return (
-    <div style={{
+   <div style={{
   backgroundImage: 'url("/background.png")',
   backgroundSize: 'cover',
-  backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
   backgroundAttachment: 'fixed',
   color: '#fff',
-  fontFamily: 'Arial, sans-serif',
+  fontFamily: 'Arial',
   padding: '20px',
-  minHeight: '100vh'
+  minHeight: '100vh',
 }}>
+
       <h1 style={{ color: '#f7c000', fontSize: '3rem', textAlign: 'center' }}>Johnny Knox</h1>
       <h2 style={{ color: '#f7c000', fontSize: '2rem', textAlign: 'center' }}>Goated Monthly</h2>
 
